45817bfe463fabb9c7859d8a31d628097e8dcd7f
thond02 <thond02@ominext.com>
2020-12-07 15:42:04 +0700

    Initial Commit

---
+++ TestCode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestCode
+//
+//  Created by Nguyen Duc Tho on 12/7/20.
+//  Copyright © 2020 Nguyen Duc Tho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
